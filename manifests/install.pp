fcda942b5747e65ac83db7b3d632c8d9f54b9fb4
Joe Cyboski <cybo42@gmail.com>
2018-02-11 21:42:33 -0500

    cleaning up warnings

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,19 +14,13 @@ class androidsdk::install(
     home       => $androidsdk_home,
     managehome => false,
     system     => true,
-  }
+  } ->
 
   file {'android_sdk_home':
     ensure => 'directory',
     path   => $androidsdk_home,
     owner  => $android_user,
-  }
-
-  # file {'/usr/local/androidsdk':
-  #   ensure => directory,
-  #   owner  => $android_user,
-  #   # group  => $android::group,
-  # } ->
+  } ->
 
   package {'unzip':
     ensure => 'present'
@@ -38,6 +32,11 @@ class androidsdk::install(
     creates => "${androidsdk_home}/tools",
     user    => $android_user,
     group   => $android_group,
+  } ->
+  exec { 'android-cli-executable':
+    command => "find ${androidsdk_home}/tools/bin/ -exec chmod 755 {} \\;",
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    cwd     => $androidsdk_home,
   }
 
   file { '/etc/profile.d/androidsdk.sh':
@@ -46,23 +45,4 @@ class androidsdk::install(
     mode    => '0644',
   }
 
-
-
-
-  # wget::fetch { 'download-android-sdk':
-    # source      => $androidsdk_source,
-    # destination => '/tmp/sdk.zip',
-  # } ->
-  # exec { 'unpack-android-sdk':
-    # command => 'unzip /tmp/sdk.zip',
-    # creates => '/usr/local/androidsdk/tools',
-    # cwd     => '/usr/local/androidsdk'
-  # }->
-  # file { 'android-executable':
-    # ensure => present,
-    # path   => "${android::paths::toolsdir}/android",
-    # owner  => $android::user,
-    # group  => $android::group,
-    # mode   => '0755',
-  # }
 }
